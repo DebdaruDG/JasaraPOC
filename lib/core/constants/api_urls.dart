29b6c7799d7ef11c830131de85690233289bf831
Debdaru Dasgupta <debdaru.dasgupta@tci.email>
2025-07-14 19:25:23 +0530

    base url changed

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -1,4 +1,6 @@
 class URLconstants {
-  static const String baseUrlRender = 'https://jasara.onrender.com/';
+  static const String baseUrlRender =
+      'https://jasara-backup-w32z.onrender.com/';
+  static const String baseUrlRenderEarlier = 'https://jasara.onrender.com/';
   static const String baseUrlAWS = '';
 }
