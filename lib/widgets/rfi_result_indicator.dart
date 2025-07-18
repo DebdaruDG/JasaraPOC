019dd1fc32d8f23423c5435daae3a41037031adc
Debdaru Dasgupta <debdaru.dasgupta@tci.email>
2025-07-18 11:07:44 +0530

    Updated Result Column PIe Chart DIagram Widget.

--- lib/widgets/rfi_result_indicator.dart
+++ lib/widgets/rfi_result_indicator.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import '../models/rfi_model.dart';
+import 'utils/app_palette.dart';
+import 'utils/app_textStyles.dart';
 
 class RFIResultIndicator extends StatelessWidget {
   final RFIModel rfi;
@@ -8,15 +10,39 @@ class RFIResultIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CircularPercentIndicator(
-      radius: 25.0,
-      lineWidth: 5.0,
-      animation: true,
-      percent: rfi.percentage / 100,
-      center: Text("${rfi.percentage}%"),
-      progressColor: rfi.progressColor,
-      backgroundColor: Colors.grey[300]!,
-      circularStrokeCap: CircularStrokeCap.round,
+    return Container(
+      padding: const EdgeInsets.all(4.0),
+      child: Column(
+        children: [
+          CircularPercentIndicator(
+            radius: 21,
+            lineWidth: 5.0,
+            animation: true,
+            percent: rfi.percentage / 100,
+            center: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Text(
+                "${rfi.percentage}%",
+                style: JasaraTextStyles.primaryText500.copyWith(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ),
+            progressColor: rfi.progressColor,
+            backgroundColor: JasaraPalette.grey.withOpacity(0.2),
+            circularStrokeCap: CircularStrokeCap.round,
+          ),
+          Text(
+            rfi.result,
+            style: JasaraTextStyles.primaryText500.copyWith(
+              fontSize: 12,
+              fontWeight: FontWeight.w800,
+              color: rfi.progressColor,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
